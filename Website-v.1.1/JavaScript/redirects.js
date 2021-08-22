9c40c281c4b116443b1edbde3b7d243896cdcbb4
Stephen Giang <stepheng753@gmail.com>
2021-08-22 15:19:41 -0700

    finished implementing slideshow to bio

--- Website-v.1.1/JavaScript/redirects.js
+++ Website-v.1.1/JavaScript/redirects.js
@@ -15,19 +15,21 @@ function sliderControls() {
 	let sliderControls = document.getElementById('sliderControls');
 	let allSlides = document.getElementById('allSlides');
 	allSlides.style.setProperty('width', allSlides.childElementCount * 100 + '%');
+
 	for (let i = 0; i < allSlides.childElementCount; i++) {
 		let circle = document.createElement('div');
 		circle.className = 'sliderCircle';
 		sliderControls.appendChild(circle);
 	}
 	let sliderCircles = sliderControls.children;
+	sliderCircles[0].style.setProperty('border-color', 'var(--title-color)');
+	sliderCircles[0].style.setProperty('background-color', 'var(--title-color)');
+
 	let movePercent = 100 / sliderCircles.length;
-	let first = document.querySelector('#firstProject');
-	let clicked = -1;
-	console.log(sliderCircles.length);
+	let first = document.querySelector('#allSlides #firstSlide');
 	for (let i = 0; i < sliderCircles.length; i++) {
 		sliderCircles[i].onclick = () => {
-			clicked = i;
+			console.log(i * movePercent);
 			first.style.setProperty('margin-left', '-' + i * movePercent + '%');
 			sliderCircles[i].style.setProperty('border-color', 'var(--title-color)');
 			sliderCircles[i].style.setProperty('background-color', 'var(--title-color)');
