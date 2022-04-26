d9ea16daaa3d433b1e107564de64c5179fa8122d
Stephen Giang <stepheng753@gmail.com>
2022-04-25 21:29:24 -0700

    Test to see if prints file correctly

--- CrossRoadsTutoring/Projects/insertCode.js
+++ CrossRoadsTutoring/Projects/insertCode.js
@@ -14,17 +14,17 @@ function convertHtmlToString(html) {
 		if (newlineIndex == -1) {
 			break;
 		}
-		let line = html.substring(i + 3, newlineIndex);
-		if (i == 0) {
-			line = html.substring(0, newlineIndex);
+		let line = i == 0 ? html.substring(0, newlineIndex) : html.substring(i + 3, newlineIndex);
+		if (line.length == 0) {
+			i = html.indexOf('<br>', newlineIndex) + 1;
+			continue;
 		}
 
-		let lineReplacement;
-		if (line.substring(0, 2) == '\t\t') {
-			lineReplacement = '<div class="indent">' + line.substring(2, line.length) + '</div>';
-		} else {
-			lineReplacement = '<div>' + line + '</div>';
-		}
+		let lineReplacement =
+			line.substring(0, 2) == '\t\t'
+				? '<div class="indent">' + line.substring(2, line.length) + '</div>'
+				: '<div>' + line + '</div>';
+
 		html = html.replace(line, lineReplacement);
 		i = html.indexOf('<br>', i) + 1;
 	}
